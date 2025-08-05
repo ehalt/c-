3574a230213b4007016a709c79773ceb3b0d2868
[-] Torikus Sadik [-] <41092966+ehalt@users.noreply.github.com>
2025-08-05 22:19:30 +0600

    ,

--- sec1.5/basics.cpp
+++ sec1.5/basics.cpp
@@ -11,3 +11,26 @@ int sum = 0, value = 0;
 
 
 
+// i have to make it more clear 
+    int currVal = 0, val = 0;
+    if (cin >> currVal) {
+        int cnt = 1;
+        while(cin >> val) {
+            if (val == currVal) {
+                ++cnt;
+            }
+            else {
+                cout << currVal << " occurs: " << cnt << "times.\n";
+                currVal = val;
+                cnt = 1;
+            }
+        }
+        cout << currVal << " occurs " << cnt << " times \n";
+    }
+
+
+
+
+
+
+
